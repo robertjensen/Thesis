7172aaf9b275b9b65cf6beaa8f643ddd1d4ef934
Robert Jensen <jensen.robert@gmail.com>
2012-02-17 15:58:30 +0100

    First working version of automated tof-analysis

--- python_figures/tof_temperature_dependence.py
+++ python_figures/tof_temperature_dependence.py
@@ -15,56 +15,48 @@ except:
 cursor = db.cursor()
 
 
-temperatures = [40,60,80,100,105,110,115,120,125,130,135]
+temperatures = [40,60,80,100,105,110,115,120,125,130,135,140,145,150,155,160,165,170,175,185,190,180,175,170,165,160,155,150,145,140,135,130,125,120,115,110,105,100]
 initial_db = 189
 
 masses = [12.43,12.77]
 
 data = {}
 
-i = 0
-for temp in temperatures:
+for i in range(0,len(temperatures)):
+    print temperatures[i]
     cursor.execute("SELECT x*1000000,y*1000 FROM xy_values_tof where measurement = " + str(i + initial_db))
-    data[str(temp)] = np.array(cursor.fetchall())
-    i = i+1
+    data[i] = np.array(cursor.fetchall())
+
 
 treated_data = {}
 for j in range(0,len(masses)):
-    treated_data[str(masses[j])] = []
+    treated_data[str(masses[j])] = np.zeros((len(temperatures),2))
 
 
-
-for temp in temperatures:
-    for j in range(0,len(masses)):
-        start = (int)((masses[j] * 2000) -100)
-        end = (int)((masses[j] * 2000) + 100)
+for i in range(0,len(masses)):
+    start = (int)((masses[i] * 2000) -50)
+    end = (int)((masses[i] * 2000) + 50)
+    for j in range(0,len(temperatures)):
         charge = 0
-        for i in range(start,end):
-            charge = charge + data[str(temp)][i,1]
-        treated_data[(str(masses[j]))].append([temp,charge])
+        for k in range(start,end):
+            charge = charge + data[j][k,1]
+        treated_data[str(masses[i])][j][0] = temperatures[j]
+        treated_data[str(masses[i])][j][1] = charge
 
 
 fig = plt.figure()
 axis_array = []
 
-print type(treated_data['12.77'])
-a = treated_data['12.77'][:][:]
-"""
+colors = ['ro-','bo-','go-']
+axis_array.append(fig.add_subplot(1,1,1))
+i = 0
+for mass in masses:    
+    axis_array[0].plot(treated_data[str(mass)][:,0],treated_data[str(mass)][:,1], colors[i])
+    i = i + 1
     
-for:    
-    axis_array.append(fig.add_subplot(1,1,i+1))
-    mass_values = ((data['hot'][:,0]-0.095) / 2.96373200801)**2.01054 
-    axis_array[i].plot(mass_values, data['hot'][:,1], 'r-')
     
-    mass_values = ((data['ambient'][:,0]-0.095) / 2.96373200801)**2.01054
-    axis_array[i].plot(mass_values, data['ambient'][:,1], 'b-')
-    axis_array[i].set_ylim(y_ranges[i])
-    
-    axis_array[i].set_xlim(x_ranges[i])
-    axis_array[i].tick_params(direction='in', length=6, width=2, colors='k',labelsize=14,axis='both',pad=5)
-    
-axis_array[0].set_ylabel('MCP Response / mV', fontsize=20)
-axis_array[0].set_xlabel('Mass / AMU', fontsize=20)
+axis_array[0].set_ylabel('Intensity', fontsize=20)
+axis_array[0].set_xlabel('Temperature', fontsize=20)
 
 
 
@@ -72,4 +64,3 @@ axis_array[0].set_xlabel('Mass / AMU', fontsize=20)
 plt.show()
 #plt.savefig('../svg_figures/oscillations_gas_dependence.svg')
 
-"""
