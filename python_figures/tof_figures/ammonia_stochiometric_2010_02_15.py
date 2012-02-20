666828dd1f5d7a5aacc2de7cfc495261f33cc04d
Robert Jensen <jensen.robert@gmail.com>
2012-02-20 22:05:35 +0100

    Various improvements to the TOF-analysis. Code still ugly...

---
+++ python_figures/tof_figures/ammonia_stochiometric_2010_02_15.py
@@ -0,0 +1,27 @@
+temperatures = []
+
+temperatures = temperatures + [40,60,80]
+
+
+for i in range(100,195,5): #Note last value is not included
+    temperatures = temperatures + [i]
+
+for i in range(180,95,-5): #Note last value is not included
+    temperatures = temperatures + [i]
+
+for i in range(100,185,5): #Note last value is not included
+    temperatures = temperatures + [i]
+
+temperatures = temperatures + [190]
+
+for i in range(180,95,-5): #Note last value is not included
+    temperatures = temperatures + [i]
+
+
+initial_db = 189
+
+masses = []
+masses.append(['M122',12.45])
+masses.append(['M1414',12.77])
+
+#masses = [12.43,12.77]
--- python_figures/tof_temperature_dependence.py
+++ python_figures/tof_temperature_dependence.py
@@ -4,7 +4,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import MySQLdb
-
+import sys
 
 try:
     db = MySQLdb.connect(host="servcinf", user="cinf_reader",passwd = "cinf_reader", db = "cinfdata")
@@ -14,51 +14,58 @@ except:
 
 cursor = db.cursor()
 
-
-temperatures = [40,60,80,100,105,110,115,120,125,130,135,140,145,150,155,160,165,170,175,185,190,180,175,170,165,160,155,150,145,140,135,130,125,120,115,110,105,100]
-initial_db = 189
-
-masses = [12.43,12.77]
+sys.path.append('tof_figures')
+import ammonia_stochiometric_2010_02_15 as config
 
 data = {}
 
-for i in range(0,len(temperatures)):
-    print temperatures[i]
-    cursor.execute("SELECT x*1000000,y*1000 FROM xy_values_tof where measurement = " + str(i + initial_db))
+for i in range(0,len(config.temperatures)):
+    print config.temperatures[i]
+    cursor.execute("SELECT x*1000000,y*1000 FROM xy_values_tof where measurement = " + str(i + config.initial_db))
     data[i] = np.array(cursor.fetchall())
 
 
 treated_data = {}
-for j in range(0,len(masses)):
-    treated_data[str(masses[j])] = np.zeros((len(temperatures),2))
+for j in range(0,len(config.masses)):
+    treated_data[str(config.masses[j][1])] = np.zeros((len(config.temperatures),2))
 
 
-for i in range(0,len(masses)):
-    start = (int)((masses[i] * 2000) -50)
-    end = (int)((masses[i] * 2000) + 50)
-    for j in range(0,len(temperatures)):
+for i in range(0,len(config.masses)):
+    start = (int)((config.masses[i][1] * 2000) -50)
+    end = (int)((config.masses[i][1] * 2000) + 50)
+    for j in range(0,len(config.temperatures)):
         charge = 0
         for k in range(start,end):
             charge = charge + data[j][k,1]
-        treated_data[str(masses[i])][j][0] = temperatures[j]
-        treated_data[str(masses[i])][j][1] = charge
+        treated_data[str(config.masses[i][1])][j][0] = config.temperatures[j]
+        treated_data[str(config.masses[i][1])][j][1] = charge
 
 
 fig = plt.figure()
-axis_array = []
 
 colors = ['ro-','bo-','go-']
-axis_array.append(fig.add_subplot(1,1,1))
+axis = fig.add_subplot(2,1,1)
+
 i = 0
-for mass in masses:    
-    axis_array[0].plot(treated_data[str(mass)][:,0],treated_data[str(mass)][:,1], colors[i])
+for mass in config.masses:    
+    axis.plot(treated_data[str(mass[1])][:,0],treated_data[str(mass[1])][:,1], colors[i])
     i = i + 1
     
     
-axis_array[0].set_ylabel('Intensity', fontsize=20)
-axis_array[0].set_xlabel('Temperature', fontsize=20)
+axis.set_ylabel('Intensity', fontsize=20)
+axis.set_xlabel('Temperature', fontsize=20)
 
+axis = fig.add_subplot(2,1,2)
+i = 0
+for mass in config.masses:    
+    axis.plot(treated_data[str(mass[1])][:,1], colors[i])
+    i = i + 1
 
+axis.set_xlabel('Tentitative time', fontsize=20)
+axis.set_ylabel('Intensity', fontsize=20)
+axis2 = axis.twinx()
+axis2.plot(treated_data[str(mass[1])][:,0], 'k.')
+axis2.set_ylabel('Temperature / C', fontsize=20)
 
 #plt.tight_layout()
 plt.show()
