45c54e6580b25836f6fd4fc0a47561624829b954
Robert Jensen <jensen.robert@gmail.com>
2012-02-22 15:18:31 +0100

    Implemented true peak fitting for much higher quality integration.

--- python_figures/tof_figures/ammonia_stochiometric_2010_02_15.py
+++ python_figures/tof_figures/ammonia_stochiometric_2010_02_15.py
@@ -8,7 +8,7 @@ for i in range(100,195,5): #Note last value is not included
 for i in range(180,95,-5): #Note last value is not included
     temperatures = temperatures + [i]
 
-for i in range(100,185,5): #Note last value is not included
+for i in range(105,185,5): #Note last value is not included
     temperatures = temperatures + [i]
 
 temperatures = temperatures + [190]
@@ -20,8 +20,11 @@ initial_db = 189
 
 masses = []
 #masses.append(['H',3.149])
-#masses.append(['H2',4.385])
-masses.append(['OH',12.4175])
-masses.append(['NH3',12.426])
-masses.append(['H2O',12.775])
+masses.append(['H2',4.385])
+masses.append(['OH',12.4193])
+masses.append(['NH3',12.428])
+masses.append(['H2O',12.777])
+masses.append(['N2',15.883])
+masses.append(['O2',16.964])
+masses.append(['N2O',19.864])
 
--- python_figures/tof_temperature_dependence.py
+++ python_figures/tof_temperature_dependence.py
@@ -1,4 +1,6 @@
 import matplotlib
+from scipy import optimize
+import math
 #matplotlib.use('svg')
 #matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -19,6 +21,7 @@ import ammonia_stochiometric_2010_02_15 as config
 
 data = {}
 
+#Get all data from the db
 for i in range(0,len(config.temperatures)):
     print config.temperatures[i]
     cursor.execute("SELECT x*1000000,y*1000 FROM xy_values_tof where measurement = " + str(i + config.initial_db))
@@ -26,29 +29,64 @@ for i in range(0,len(config.temperatures)):
 
 
 treated_data = {}
-for j in range(0,len(config.masses)):
-    treated_data[str(config.masses[j][1])] = np.zeros((len(config.temperatures),2))
+#Prepare the list that will hold the (temp,intensity) values
+for mass in config.masses:
+    treated_data[mass[0]] = np.zeros((len(config.temperatures),2))
+
+
 
 
 for i in range(0,len(config.masses)):
-    start = (int)((config.masses[i][1] * 2000) -5)
-    end = (int)((config.masses[i][1] * 2000) + 5)
+
+    #axis = fig.add_subplot(4,3,i+1)
+    center = (int)(config.masses[i][1] * 2000) ## Notice... 
+    center_mass = config.masses[i][1]
+    start = center - 8
+    end = center + 8
     for j in range(0,len(config.temperatures)):
+
+        x_values = data[j][start:end,0]
+        y_values = data[j][start:end,1]
+        
+        # Fit the first set
+        fitfunc = lambda p, x: p[0]*math.e**(-1*((x-center_mass-p[2])**2)/p[1])       # Target function
+        errfunc = lambda p, x, y: fitfunc(p, x) - y # Distance to the target function
+        p0 = [50,0.00001,0] # Initial guess for the parameters
+        p1, success = optimize.leastsq(errfunc, p0[:], args=(x_values, y_values))        
+
+        if (success > 4) or p1[1]>0.01:
+            p0 = [5,0.00001,0]
+            p1, success = optimize.leastsq(errfunc, p0[:], args=(x_values[2:7], y_values[2:7]))
+            
+        if (success > 4) or p1[1]>0.01:
+            p0 = [5,0.00001,0]
+            p1, success = optimize.leastsq(errfunc, p0[:], args=(x_values[2:5], y_values[2:5]))
+       
+        if (success > 4) or p1[1]>0.01:
+            print "p1:" + str(p1[0]) + " p1:" + str(p1[1]) + " p2:" + str(p1[2]) + " j: " + str(j)
+            p1[1] = 0
+
+        #axis.plot(data[j][start-40:end+60,0],data[j][start-40:end+60,1],'b-')
+        #axis.plot([data[j][start,0],data[j][start,0]],[0,100])
+        #axis.plot([data[j][end,0],data[j][end,0]],[0,100])
+
+        #axis.plot(data[j][start-40:end+40,0],fitfunc(p1, data[j][start-40:end+40,0]),'r-')
+        
         charge = 0
-        for k in range(start,end):
-            charge = charge + data[j][k,1]
-        treated_data[str(config.masses[i][1])][j][0] = config.temperatures[j]
-        treated_data[str(config.masses[i][1])][j][1] = charge
+        treated_data[config.masses[i][0]][j][0] = config.temperatures[j]
+        treated_data[config.masses[i][0]][j][1] = p1[0] * math.sqrt(p1[1])
+
+#plt.show()
 
 
+colors = ['ro-','bo-','go-','co-','mo-','yo-','r*-','b*-','g*-']
 fig = plt.figure()
-
-colors = ['ro-','bo-','go-']
 axis = fig.add_subplot(2,1,1)
+fig.subplots_adjust(hspace=0.3)
 
 i = 0
 for mass in config.masses:    
-    axis.plot(treated_data[str(mass[1])][:,0],treated_data[str(mass[1])][:,1], colors[i],label=str(mass[0]))
+    axis.plot(treated_data[mass[0]][:,0],treated_data[mass[0]][:,1], colors[i],label=mass[0])
     i = i + 1
     
     
@@ -58,14 +96,14 @@ axis.legend()
 axis = fig.add_subplot(2,1,2)
 i = 0
 for mass in config.masses:    
-    axis.plot(treated_data[str(mass[1])][:,1], colors[i],label=str(mass[0]))
+    axis.plot(treated_data[mass[0]][:,1], colors[i],label=mass[0])
     i = i + 1
 
-axis.set_xlabel('Tentitative time', fontsize=20)
+axis.set_xlabel('Tentative time', fontsize=20)
 axis.set_ylabel('Intensity', fontsize=20)
 
 axis2 = axis.twinx()
-axis2.plot(treated_data[str(mass[1])][:,0], 'k.',label='Temperature')
+axis2.plot(treated_data[mass[0]][:,0], 'k.',label='Temperature')
 axis2.set_ylabel('Temperature / C', fontsize=20)
 
 axis.legend()
@@ -73,4 +111,3 @@ axis.legend()
 #plt.tight_layout()
 plt.show()
 #plt.savefig('../svg_figures/oscillations_gas_dependence.svg')
-
