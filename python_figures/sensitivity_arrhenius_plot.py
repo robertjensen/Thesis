e7ca3966d593791eddef62f86378a965e3515b21
Robert Jensen <jensen.robert@gmail.com>
2012-05-22 20:06:39 +0200

    Work on TOF and sensitivity chapters

--- python_figures/sensitivity_arrhenius_plot.py
+++ python_figures/sensitivity_arrhenius_plot.py
@@ -1,4 +1,4 @@
-#backend = 'svg'
+backend = 'svg'
 execfile('std_header.py')
 
 import sensitivity_parameters as sens_parm
@@ -18,8 +18,8 @@ data = {}
 fig = plt.figure()
 fig.subplots_adjust(left=d.left_room) 
 fig.subplots_adjust(bottom=d.bottom_room)
-fig.subplots_adjust(right=d.right_room) 
-fig.subplots_adjust(hspace=0.2)
+fig.subplots_adjust(right=d.right_room*0.95) 
+fig.subplots_adjust(hspace=0.1)
 fig.subplots_adjust(wspace=0.1)
 
 ratio = 1
@@ -31,8 +31,9 @@ fig.set_size_inches(fig_width,fig_height)
 oxygen_factor = 8.2e-3
 bg = 0.12
 
-axis = fig.add_subplot(1,1,1)
 
+axis = fig.add_subplot(2,1,1)
+axis_sub = fig.add_subplot(2,1,2)
 for i in range(0,len(parameters)):
     p = parameters[i]
     print p.name
@@ -51,7 +52,7 @@ for i in range(0,len(parameters)):
     y_values = data['M44'][:,1]-bg-oxygen_factor*m32_interp(data['M44'][:,0])
     m44_interp = interpolate.interp1d(data['M44'][:,0],y_values, bounds_error=False)
 
-    axis.plot(x_values, y_values, p.color)
+    #axis.plot(x_values, y_values, p.color)
     
     fit_x = np.linspace(p.fitx[0]/60000,p.fitx[1]/60000,100)
     fit_x_values = 273.15+temp_interp(fit_x)
@@ -62,24 +63,58 @@ for i in range(0,len(parameters)):
     p1, success = optimize.leastsq(errfunc, p0[:], args=(fit_x_values,m44_interp(fit_x)),maxfev=1000)
     print p1
     #axis.plot(1000.0/fit_plot_values, fitfunc(p1,fit_plot_values), 'k-')
-    axis.plot(1000.0/fit_x_values, fitfunc(p1,fit_x_values), 'k-',linewidth=3)
+    axis.plot(1000.0/fit_x_values, np.log10(fitfunc(p1,fit_x_values)), 'k-',linewidth=2)
     p.ae = p1[1]
     p.prefactor = p1[0]
     #print temp_interp(fit_x)
-    axis.plot(x_values, y_values, p.color)
+    axis.plot(x_values, np.log10(y_values), p.color)
+    
+    axis_sub.plot(x_values, np.log10(y_values/p.area), p.color)
 
     
-
-axis.set_yscale('log')
+yticks = np.array([0.1,1,10])
+axis.set_yticks(np.log10(yticks))
+axis.set_yticklabels(yticks)
 axis.set_xlim(1.4,2.4)
-axis.set_ylim(1e-1,20)
-
+axis.set_ylim(np.log10(1e-1),np.log10(20))
 axis.set_ylabel('SEM Current / nA', fontsize=d.y_axis_font)
-axis.set_xlabel('1000K / Temperature', fontsize=d.y_axis_font)
+axis.set_xticklabels([])
 
+axis2 = axis.twinx()
+yticks = np.array([1e12,1e13,1e14])
+yticks_s = ["10$^{12}$","10$^{13}$","10$^{14}$"]
+axis2.set_ylim(np.log10(1e-1),np.log10(20))
+axis2.set_yticks(np.log10(yticks*1.6e-13))
+axis2.set_yticklabels(yticks_s)
+axis2.set_ylabel('TOF / s$^{-1}$', fontsize=d.y_axis_font)
+axis2.set_xticklabels([])
 axis.tick_params(direction='in', length=d.ticklength, width=2, colors='k',labelsize=d.labelsize,axis='both',pad=d.pad)
-plt.show()
-#plt.savefig('../svg_figures/sensitivity_background_correction_complete.svg')
+axis2.tick_params(direction='in', length=d.ticklength, width=2, colors='k',labelsize=d.labelsize,axis='both',pad=d.pad)
+
+## Setup of second plot
+yticks = np.array([1e-7,1e-6,1e-5,1e-4,1e-3,1e-2,1e-1])
+yticks_s = ["10$^{-7}$","10$^{-6}$","10$^{-5}$","10$^{-4}$","10$^{-3}$","10$^{-2}$","10$^{-1}$"]
+axis_sub.set_ylim(-7.1,-0.9)
+axis_sub.set_yticks(np.log10(yticks))
+axis_sub.set_yticklabels(yticks_s)
+axis_sub.set_xlim(1.4,2.4)
+axis_sub.set_ylabel('SEM/area / nA/($\mu$m)$^2$', fontsize=d.y_axis_font)
+axis_sub.set_xlabel('1000K / Temperature', fontsize=d.y_axis_font)
+
+axis2_sub = axis_sub.twinx()
+yticks = np.array([1e6,1e7,1e8,1e9,1e10,1e11,1e12])
+yticks_s = ["10$^{6}$","10$^{7}$","10$^{8}$","10$^{9}$","10$^{10}$","10$^{11}$","10$^{12}$"]
+axis2_sub.set_ylim(-7.1,-0.9)
+axis2_sub.set_yticks(np.log10(yticks*1.6e-13))
+axis2_sub.set_yticklabels(yticks_s)
+axis2_sub.set_ylabel('TOF/area / s$^{-1}$ $\cdot$ ($\mu$m)$^{-2}$', fontsize=d.y_axis_font)
+
+axis_sub.tick_params(direction='in', length=d.ticklength, width=2, colors='k',labelsize=d.labelsize,axis='both',pad=d.pad)
+axis2_sub.tick_params(direction='in', length=d.ticklength, width=2, colors='k',labelsize=d.labelsize,axis='both',pad=d.pad)
+
+
+#plt.show()
+plt.savefig('../svg_figures/sensitivity_arrhenius.svg')
 
 del fig
 
@@ -98,28 +133,36 @@ ae_list_down =  np.array(ae_list_down)
 fig = plt.figure()
 fig.subplots_adjust(left=d.left_room) 
 fig.subplots_adjust(bottom=d.bottom_room)
-fig.subplots_adjust(right=d.right_room) 
+fig.subplots_adjust(right=d.right_room*0.95) 
 fig.subplots_adjust(hspace=0.2)
 fig.subplots_adjust(wspace=0.1)
 
-ratio = 1
+ratio = d.ratio
 fig_width = d.width
 fig_width = fig_width /2.54     # width in cm converted to inches
 fig_height = fig_width*ratio
 fig.set_size_inches(fig_width,fig_height)
 axis = fig.add_subplot(1,1,1)
 
-axis.plot(ae_list_up[:,0],ae_list_up[:,1], 'ko-')
-axis.plot(ae_list_down[:,0],ae_list_down[:,1], 'k*--')
-axis.set_ylim(0,20000)
-axis.set_ylabel('Activation Energy / ', fontsize=d.y_axis_font)
+Kb = 8.617e-5
+
+axis.plot(ae_list_up[:,0],ae_list_up[:,1]*Kb, 'ko-')
+axis.plot(ae_list_down[:,0],ae_list_down[:,1]*Kb, 'k*-')
+axis.set_ylim(0,2)
+axis.set_ylabel('Activation Energy / eV', fontsize=d.y_axis_font)
 axis.set_xlabel('Pt spot area / ($\mu$m)$^2$', fontsize=d.y_axis_font)
 axis.set_xscale('log')
 
 axis2 = axis.twinx()
-axis2.plot(ae_list_up[:,0],ae_list_up[:,2], 'ro-')
-axis2.plot(ae_list_down[:,0],ae_list_down[:,2], 'r*--')
-axis2.set_ylabel('Prefactor / au.', fontsize=d.y_axis_font)
+axis2.plot(ae_list_up[:,0],ae_list_up[:,2]*1.6e-13, 'ro-')
+axis2.plot(ae_list_down[:,0],ae_list_down[:,2]*1.6e-13, 'r*-')
+axis2.set_ylabel('Prefactor / s$^{-1}$.', fontsize=d.y_axis_font)
 axis2.set_yscale('log')
 axis2.set_xscale('log')
-plt.show()
+
+axis.tick_params(direction='in', length=d.ticklength, width=2, colors='k',labelsize=d.labelsize,axis='both',pad=d.pad)
+axis2.tick_params(direction='in', length=d.ticklength, width=2, colors='k',labelsize=d.labelsize,axis='both',pad=d.pad)
+
+
+#plt.show()
+plt.savefig('../svg_figures/sensitivity_activation_energies.svg')
---
+++ python_figures/tof_benzene_hydrogenation.py
@@ -0,0 +1,144 @@
+backend = 'svg'
+execfile('std_header.py')
+
+#Mass of NH3: 17.0265491
+#Mass of OH: 17.0027397
+
+#Correct this expression to spectrum 403
+def MassToTime(mass):
+    time = 2.97277909899 * (mass**0.499352719883)
+    corr_time = time + 0.153#Not adapted to 403
+    corr_time = time + 0.220
+    return corr_time 
+
+    time = 2.98660484819 * (mass**0.4977024241071)
+    corr_time = time + 0.153
+    return corr_time 
+
+
+fig = plt.figure()
+fig.subplots_adjust(left=d.left_room) 
+fig.subplots_adjust(bottom=d.bottom_room)
+fig.subplots_adjust(right=d.right_room) 
+fig.subplots_adjust(hspace=0.3)
+
+ratio = 1
+fig_width = d.width
+fig_width = fig_width /2.54     # width in cm converted to inches
+fig_height = fig_width*ratio
+fig.set_size_inches(fig_width,fig_height)
+
+
+data = {}
+#Benzene in He
+cursor.execute("SELECT x*1000000,y*1000 FROM xy_values_tof where measurement = 323")
+data['pure'] = np.array(cursor.fetchall())
+#Benzene in He
+cursor.execute("SELECT x*1000000,y*1000 FROM xy_values_tof where measurement = 324")
+data['hydrogen'] = np.array(cursor.fetchall())
+
+axis_array = []
+
+LINEWIDTH = 0.5
+arrow = dict(facecolor='black',arrowstyle='->')
+font = d.arrowfont
+
+gs = gridspec.GridSpec(2, 3)
+gs.update(wspace=0.2,hspace=0.4)
+
+axis = plt.subplot(gs[0, :])
+
+axis.plot(data['pure'][:,0], data['pure'][:,1], 'r-',linewidth=LINEWIDTH)
+axis.plot(data['hydrogen'][:,0], data['hydrogen'][:,1], 'b-',linewidth=LINEWIDTH)
+axis.set_xlim(0,28)
+axis.set_xticks([5,10,15,20,25])
+
+mass_ticks = np.array([1,2,5,10,20,30,40,50,60,70,80])
+axis3 = axis.twiny()
+axis3.set_xlim(0,28)
+print MassToTime(2*1.0078250320)
+axis3.set_xticks(MassToTime(mass_ticks))
+axis3.set_xticklabels(mass_ticks)
+axis3.set_xlabel('Mass / amu', fontsize=d.x_axis_font)
+axis3.tick_params(direction='in', length=d.ticklength, width=2, colors='k',labelsize=d.labelsize,axis='both',pad=d.pad)
+#axis3.ticklabel_format(useOffset=False)
+axis.set_ylim(-10,450)
+axis.set_yticks((0,100,200,300,400))
+
+p = axis.axvspan(26, 27.7, facecolor='#26aaf7', alpha=0.25)
+#p = axis.axvspan(15, 20, facecolor='#25dd37', alpha=0.25)
+#p = axis.axvspan(26, 27.7, facecolor='#b6fa77', alpha=0.25)
+
+axis.set_ylabel('Response / mV', fontsize=d.y_axis_font)
+#axis.set_xlabel('Flight Time / $\mu$s', fontsize=d.y_axis_font)
+axis.set_xlabel('')
+axis.tick_params(direction='in', length=d.ticklength, width=2, colors='k',labelsize=d.labelsize,axis='both',pad=d.pad)
+
+
+"""
+axis = plt.subplot(gs[1,0])
+axis.set_ylabel('Response / mV', fontsize=d.y_axis_font)
+axis.set_yticks((0,50,100,150,200))
+axis.plot(data['pure'][:,0], data['pure'][:,1], 'r-',linewidth=LINEWIDTH)
+axis.plot(data['hydrogen'][:,0], data['hydrogen'][:,1], 'b-',linewidth=LINEWIDTH)
+p = axis.axvspan(0, 1, facecolor='#26aaf7', alpha=0.25)
+axis.set_xlim(4.35,4.45)
+axis.set_ylim(0,225)
+axis.set_xticks([4.35,4.4,4.45])
+axis.tick_params(direction='in', length=d.ticklength, width=2, colors='k',labelsize=d.labelsize,axis='both',pad=d.pad)
+
+axis = plt.subplot(gs[1,1])
+axis.set_ylabel('')
+axis.set_yticks((10,20,30,40,50,60,70,80,90))
+#axis.set_xticks((3.25,3.75,4.25))
+axis.plot(data['pure'][:,0], data['pure'][:,1], 'r-',linewidth=LINEWIDTH)
+axis.plot(data['hydrogen'][:,0], data['hydrogen'][:,1], 'b-',linewidth=LINEWIDTH)
+p = axis.axvspan(3.0, 4.5, facecolor='#25dd37', alpha=0.25)
+axis.set_xlim(15,20)
+axis.set_ylim(0,100)
+axis.tick_params(direction='in', length=d.ticklength, width=2, colors='k',labelsize=d.labelsize,axis='both',pad=d.pad)
+axis.ticklabel_format(useOffset=False)
+#axis.annotate('H', xy=(3.2, 10),  xycoords='data', xytext=(3.5, 15), textcoords='data', arrowprops=arrow, horizontalalignment='right', verticalalignment='top',fontsize=font,)
+#axis.annotate('H$_2$', xy=(4.35, 10),  xycoords='data', xytext=(4.2, 20), textcoords='data', arrowprops=arrow, horizontalalignment='right', verticalalignment='top',fontsize=font,)
+axis.set_xlabel('Flight Time / $\mu$s', fontsize=d.x_axis_font)
+"""
+
+#axis = plt.subplot(gs[1,2])
+axis = plt.subplot(gs[1,:])
+axis.set_ylabel('')
+axis.set_yticks((50,100,150,200,250,300,350,400,450))
+#axis.set_xticks((12,12.5))
+axis.plot(data['pure'][:,0], data['pure'][:,1], 'r-',linewidth=LINEWIDTH)
+axis.plot(data['hydrogen'][:,0], data['hydrogen'][:,1], 'b-',linewidth=LINEWIDTH)
+axis.set_xlim(26,27.7)
+axis.set_ylim(-10,450)
+
+#axis.set_xticks([12.1,12.2,12.3,12.4])
+axis.ticklabel_format(useOffset=False)
+#p = axis.axvspan(26, 27.7, facecolor='#b6fa77', alpha=0.25)
+axis.tick_params(direction='in', length=d.ticklength, width=2, colors='k',labelsize=d.labelsize,axis='both',pad=d.pad)
+
+mass_ticks = np.array([76,77,78,79,80,81,82,83,84,85])
+axis3 = axis.twiny()
+axis3.set_xlim(26,27.7)
+print MassToTime(2*1.0078250320)
+axis3.set_xticks(MassToTime(mass_ticks))
+axis3.set_xticklabels(mass_ticks,size=60)
+
+axis3.tick_params(direction='in', length=d.ticklength, width=2, colors='k',labelsize=d.labelsize,axis='x',pad=d.pad)
+for tl in axis3.get_xticklabels():
+    tl.set_size(8)
+
+axis.set_yticks((0,100,200,300,400))
+axis.set_ylabel('Response / mV', fontsize=d.y_axis_font)
+axis.set_xlabel('Flight Time / $\mu$s', fontsize=d.y_axis_font)
+
+#axis.annotate('O', xy=(12.04, 10),  xycoords='data', xytext=(12.04, 67), textcoords='data', arrowprops=arrow, horizontalalignment='right', verticalalignment='top',fontsize=font,)
+#axis.annotate('NH$_2$', xy=(12.055, 47),  xycoords='data', xytext=(12.2, 75), textcoords='data', arrowprops=arrow, horizontalalignment='right', verticalalignment='top',fontsize=font,)
+#axis.annotate('OH', xy=(12.414, 50),  xycoords='data', xytext=(12.35, 66), textcoords='data', arrowprops=arrow, horizontalalignment='right', verticalalignment='top',fontsize=font,)
+#axis.annotate('NH$_3$', xy=(12.425, 85),  xycoords='data', xytext=(12.35, 130), textcoords='data', arrowprops=arrow, horizontalalignment='right', verticalalignment='top',fontsize=font,)
+
+
+#plt.tight_layout()
+#plt.show()
+plt.savefig('../svg_figures/tof_benzene_hydrogenation.svg')
